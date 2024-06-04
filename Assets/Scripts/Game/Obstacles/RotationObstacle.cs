8f5c60e36dc109c2c5dbe71b166a00a79daecfe5
Anton Vorobev <anonspirov@yandex.ru>
2024-06-04 10:14:17 +0400

    rotation obstacle

---
+++ Assets/Scripts/Game/Obstacles/RotationObstacle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game.Obstacles
+{
+	public class RotationObstacle : Obstacle
+	{
+		[SerializeField]
+		private float _rotationSpeed = .2f;
+		[SerializeField]
+		private bool _clockwise;
+
+		private void Update()
+		{
+			var rotation = new Vector3(
+				0f,
+				this._clockwise ? this._rotationSpeed : -this._rotationSpeed,
+				0f);
+			
+			this.transform.Rotate(rotation);
+		}
+	}
+}
